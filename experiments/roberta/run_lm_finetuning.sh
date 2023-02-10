174a36e0b2c520b8c7b81db84d65d1530e4391cc
Mayuko-Kimura <56258264+Mayuko-Kimura@users.noreply.github.com>
2023-02-10 18:12:32 +0900

    Delete run_lm_finetuning.sh

--- experiments/roberta/run_lm_finetuning.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-python experiments/roberta/run_lm_finetuning.py \
-  --train_file data_mlm/mlm_input_yes.txt \
-  --bert_model roberta-base \
-  --do_lower_case \
-  --do_train \
-  --output_dir mlm_roberta 
