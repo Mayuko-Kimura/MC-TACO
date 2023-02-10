7f723bf28eb86119175eba4e3b72b9d2a69d5266
Mayuko-Kimura <56258264+Mayuko-Kimura@users.noreply.github.com>
2023-02-10 18:10:11 +0900

    Delete experiments/bert/git載せない directory

--- experiments/bert/git載せない/run_lm_finetuning-nsp.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-python experiments/bert/run_lm_finetuning-nsp.py \
-       --train_file data_mlm/mlm_input_yes.txt \
-       --bert_model bert-base-uncased \
-       --do_lower_case \
-       --do_train \
-       --output_dir mlm
